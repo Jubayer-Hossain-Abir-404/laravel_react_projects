65d7d9ef5d55fc0bc1725d91d4046f208c3e7a6b
Md. Jubayer Hossain Abir <41603187+Jubayer-Hossain-Abir-404@users.noreply.github.com>
2023-12-19 22:11:22 +0600

    students data view

--- react_crud/src/pages/Student.js
+++ react_crud/src/pages/Student.js
@@ -1,14 +1,59 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 function Student(){
+
+    const [loading, setLoading] = useState(true);
+    const [students, setStudents] = useState([]);
+
+    useEffect(() => {
+        axios.get(`http://127.0.0.1:8000/api/students`).then(res => {
+            console.log(res);
+            setStudents(res.data.students);
+            setLoading(false);
+        });
+    }, []);
+
+
+    if(loading){
+        return (
+            <div>
+                Loading...
+            </div>
+        )
+    }
+
+
+    var studentDetails = "";
+    studentDetails = students.map( (item, index) => {
+        return (
+            <tr key={index}>
+                <td>{item.id}</td>
+                <td>{item.name}</td>
+                <td>{item.course}</td>
+                <td>{item.email}</td>
+                <td>{item.phone}</td>
+                <td>
+                    <Link to="/" className="btn btn-success">Edit</Link>
+                </td>
+                <td>
+                    <Link to="/" className="btn btn-danger">Delete</Link>
+                </td>
+            </tr>
+        )
+    });
+
     return (
-      <div className="container">
+      <div className="container mt-5">
         <div className="row">
           <div className="col-md-12">
             <div className="card">
               <div className="card-header">
                 <h4>Students List</h4>
-                <Link to="/">Add Student</Link>
+                <Link to="/" className="btn btn-primary float-end">
+                  Add Student
+                </Link>
               </div>
               <div className="card-body">
                 <table className="table table-striped">
@@ -23,9 +68,7 @@ function Student(){
                       <th>Delete</th>
                     </tr>
                   </thead>
-                  <tbody>
-                    
-                  </tbody>
+                  <tbody>{studentDetails}</tbody>
                 </table>
               </div>
             </div>
