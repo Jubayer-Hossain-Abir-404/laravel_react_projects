069260a36d2fdae241b6bd01abde083f05dac950
Md. Jubayer Hossain Abir <41603187+Jubayer-Hossain-Abir-404@users.noreply.github.com>
2024-03-14 20:57:01 +0600

    filter button, todolist component , dispatch, useselect use

---
+++ react_redux_to_do_app/src/components/TodoList.jsx
@@ -0,0 +1,19 @@
+import { useSelector } from "react-redux"
+
+
+const TodoList = () => {
+    // const filteredTodos = useSelector((state) => {
+    //     const todos = state.todos;
+    //     const filter = state.filter;
+    //     const searchTerm = state.searchTerm;
+
+    //     return todos.filter((todo) => {
+    //         const matchsFilter = (filter === "COMPLETED" && todo.completed)
+    //     })
+    // })
+  return (
+    <div>TodoList</div>
+  )
+}
+
+export default TodoList
