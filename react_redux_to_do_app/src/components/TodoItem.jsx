7afe120045f428e27cb7c1d6ef993e3c6e749e6e
Md. Jubayer Hossain Abir <41603187+Jubayer-Hossain-Abir-404@users.noreply.github.com>
2024-03-18 20:31:04 +0600

    todolist is being displayed with toggle button

---
+++ react_redux_to_do_app/src/components/TodoItem.jsx
@@ -0,0 +1,19 @@
+import { FaToggleOff, FaToggleOn } from 'react-icons/fa'
+
+const TodoItem = ({todo, index}) => {
+  return (
+    <li className="flex flex-col sm:flex-row sm:items-center justify-between border-b-2 py-2 gap-4">
+        <div className="flex items-center">
+            <span className="mr-4 text-gray-500">{index + 1}</span>
+            <span className={`mr-4 ${todo.completed ? "line-through text-red-500": " "}`}>{todo.text}</span>
+        </div>
+        <div className="space-x-3 ml-8">
+            <button className='mr-2 text-sm bg-blue-500 text-white sm:px-2 py-1 px-1 rounded'>
+                {todo.completed ? <FaToggleOff /> : <FaToggleOn /> }
+            </button>
+        </div>
+    </li>
+  )
+}
+
+export default TodoItem
--- react_redux_to_do_app/src/components/TodoList.jsx
+++ react_redux_to_do_app/src/components/TodoList.jsx
@@ -1,18 +1,31 @@
 import { useSelector } from "react-redux"
+import TodoItem from "./TodoItem";
 
 
 const TodoList = () => {
-    // const filteredTodos = useSelector((state) => {
-    //     const todos = state.todos;
-    //     const filter = state.filter;
-    //     const searchTerm = state.searchTerm;
+    const filteredTodos = useSelector((state) => {
+        const todos = state.todos;
+        const filter = state.filter;
+        const searchTerm = state.searchTerm;
 
-    //     return todos.filter((todo) => {
-    //         const matchsFilter = (filter === "COMPLETED" && todo.completed)
-    //     })
-    // })
+        return todos.filter((todo) => {
+            const matchsFilter = (filter === "COMPLETED" && todo.completed) || (filter === "INCOMPLETE" && !todo.completed)
+            || (filter === "ALL");
+            const matchsSearch = todo.text.toLowerCase().includes(searchTerm);
+
+            return matchsFilter && matchsSearch;
+        })
+    })
+    //console.log('Filtered Todos:', filteredTodos);
   return (
-    <div>TodoList</div>
+    <ul>
+      <li className="my-2 text-sm italic">All Your Notes Here...</li>
+      {
+        filteredTodos.map((todo, index) =>(
+          <TodoItem  key={index} todo={todo} index={index}/>
+        ))
+      }
+    </ul>
   )
 }
 
